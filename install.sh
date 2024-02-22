e22fe03425c2462dcfeec0f373bd32cc6ea1f54b
0x7gen <nilangaperera@live.com>
2024-02-23 07:46:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,31 +10,31 @@ apt update
 
 # This command will install whois tool:
 echo -e "${BIBlue} [+] Installing whois..."
-apt install whois
+apt install whois -y
 
 # This command will install sublist3r tool:
 echo -e "${BIBlue} [+] Installing sublist3r..."
-apt install sublist3r
+apt install sublist3r -y
 
 # This command will install subfinder tool:
 echo -e "${BIBlue} [+] Installing subfinder..."
-apt install subfinder
+apt install subfinder -y
 
 # This command will install assetfinder tool:
 echo -e "${BIBlue} [+] Installing assetfinder..."
-apt install assetfinder
+apt install assetfinder -y
 
 # This command will install amass tool:
 echo -e "${BIBlue} [+] Installing amass..."
-apt install amass
+apt install amass -y
 
 # This command will install httprobe tool:
 echo -e "${BIBlue} [+] Installing httprobe..."
-apt install httprobe
+apt install httprobe -y
 
 # This command will install eyewitness tool:
 echo -e "${BIBlue} [+] Installing eyewitness..."
-apt install eyewitness
+apt install eyewitness -y
 
 
 echo -e "${Yellow} [+] Installation has been successfully completed!"
